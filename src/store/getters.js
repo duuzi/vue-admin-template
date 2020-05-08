f6c4cc318075d8c39b4f0801bf6e240d4c6ef8ef
24174 <24174@WXCNL-2958>
2020-05-08 16:33:16 +0800

    新增tagsview

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   token: state => state.user.token,
   avatar: state => state.user.avatar,
   name: state => state.user.name,
-  menulist:state=>state.user.menulist
+  menulist:state=>state.user.menulist,
+  visitedviews: state => state.tagsview.visitedviews // 新增
 }
 export default getters
